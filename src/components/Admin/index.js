e61664f55a29719d0e5a9bb64fb9eb21bd1a2281
Ganesan Gurumurthy <ganesansays@gmail.com>
2018-12-31 22:35:12 +0530

    added user listing

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,18 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import * as ROLES from '../../constants/roles';
-import { withAuthorization } from '../Session';
+import { withFirebase } from '../Firebase';
 
-const AdminPage = () => (
-  <div>
-    <h1>Admin</h1>
-    <p>
-      Restricted area! Only users with the admin role are authorized. {ROLES.ADMIN}
-    </p>
-  </div>
-);
+class AdminPage extends Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      loading: false,
+      users: [],
+    };
+  }
+  
+  componentDidMount() {
+    this.setState({ loading: true });
+  
+    this.props.firebase.users().on('value', snapshot => {
+      const usersObject = snapshot.val();
+  
+      const usersList = Object.keys(usersObject).map(key => ({
+        ...usersObject[key],
+        uid: key,
+      }));
+  
+      this.setState({
+        users: usersList,
+        loading: false,
+      });
+    });
+  }
 
-const condition = authUser =>
-  authUser && authUser.roles && authUser.roles.includes(ROLES.ADMIN);
+  componentWillUnmount() {
+    this.props.firebase.users().off();
+  }
 
-export default withAuthorization(condition)(AdminPage);
+  render() {
+    const { users, loading } = this.state;
+    return (
+      <div>
+        <h1>Admin</h1>
+
+        {loading && <div>Loading ...</div>}
+
+        <UserList users={users} />
+      </div>
+    );
+  }
+}
+
+const UserList = ({ users }) => (
+    <ul>
+      {users.map(user => (
+        <li key={user.uid}>
+          <span>
+            <strong>ID:</strong> {user.uid}
+          </span>
+          <span>
+            <strong>E-Mail:</strong> {user.email}
+          </span>
+          <span>
+            <strong>Username:</strong> {user.username}
+          </span>
+        </li>
+      ))}
+    </ul>
+  );
+
+export default withFirebase(AdminPage);
